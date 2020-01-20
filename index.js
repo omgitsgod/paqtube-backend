c2877824b8b7811b2adc93283dd17d0ec606a6c9
omgitsgod <omgitsgod@gmail.com>
2020-01-19 22:29:47 -0500

    Working backend

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const fetch   = require('node-fetch');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+const url = 'https://www.googleapis.com/youtube/v3/search';
+const key = process.env.KEY2;
+const channelId = 'UCvO6uJUVJQ6SrATfsWR5_aA';
+const part = 'snippet';
+const type = 'video';
+const maxResults = 30
+let pageToken = null
+
+app.get('/', cors(), (req, res) => {
+  fetch(`${url}?key=${key}&channelId=${channelId}&type=${type}&part=${part}&maxResults=${maxResults}`)
+  .then(r => r.json())
+  .then(json => {
+    pageToken = json.nextPageToken
+    console.log(json)
+    res.json(json)
+  });
+});
+
+app.get('/page/:token', cors(), (req, res) => {
+  const token = req.params.token
+  fetch(`${url}?key=${key}&channelId=${channelId}&type=${type}&part=${part}&maxResults=${maxResults}&pageToken=${token}`)
+  .then(r => r.json())
+  .then(json => {
+    pageToken = json.nextPageToken
+    console.log(json)
+    res.json(json)
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
